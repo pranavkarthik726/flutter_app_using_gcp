fdaa7f1561a7611d1261210d91cd4c883da1a5f8
pranavkarthik726 <167102866+pranavkarthik726@users.noreply.github.com>
2024-07-05 15:41:54 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app"
+  "FLUTTER_TARGET=C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\prana\\Downloads\\image_prediction_app (2)\\image_prediction_app\\.dart_tool\\package_config.json"
+)
